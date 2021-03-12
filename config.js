12228e89983274fe32e4f57228f7171207856d7b
Joost Wooning <jwooning@gmail.com>
2021-03-12 16:36:56 +0100

    Apple login configuration (#3)
    * Apple login configuration
    
    * github authentication
    
    * 1.2.0

--- config.js
+++ config.js
@@ -1,24 +1,44 @@
 import document from 'global/document';
 import qs from 'querystringify';
 
-export const GOOGLE_AUTH_ENDPOINT = 'https://accounts.google.com/o/oauth2/auth';
-export const GOOGLE_CLIENT_ID = '45471411055-ornt4svd2miog6dnopve7qtmh5mnu6id.apps.googleusercontent.com';
-export const GOOGLE_URL_ROOT = 'https://www.googleapis.com/';
-
 var redirectOrigin = 'http://127.0.0.1';
 if (document.location) {
   redirectOrigin = document.location.origin;
 }
-export const REDIRECT_URI = redirectOrigin + '/auth/g/redirect';
 
-export const OAUTH_PARAMS = {
+export const GOOGLE_CLIENT_ID = '45471411055-ornt4svd2miog6dnopve7qtmh5mnu6id.apps.googleusercontent.com';
+export const GOOGLE_AUTH_ENDPOINT = 'https://accounts.google.com/o/oauth2/auth';
+export const GOOGLE_URL_ROOT = 'https://www.googleapis.com/';
+export const GOOGLE_REDIRECT_PATH = '/auth/g/redirect';
+export const GOOGLE_REDIRECT_URI = redirectOrigin + GOOGLE_REDIRECT_PATH;
+export const GOOGLE_OAUTH_PARAMS = {
   type: 'web_server',
   client_id: GOOGLE_CLIENT_ID,
-  redirect_uri: REDIRECT_URI,
+  redirect_uri: GOOGLE_REDIRECT_URI,
   response_type: 'code',
   scope: 'https://www.googleapis.com/auth/userinfo.email',
   prompt: 'select_account',
 };
+export const GOOGLE_REDIRECT_LINK = [GOOGLE_AUTH_ENDPOINT, qs.stringify(GOOGLE_OAUTH_PARAMS)].join('?')
 
+export const APPLE_AUTH_ENDPOINT = 'https://appleid.apple.com/auth/authorize';
+export const APPLE_CLIENT_ID = 'ai.comma.login';
+export const APPLE_REDIRECT_PATH = '/auth/a/redirect';
+export const APPLE_REDIRECT_URI = 'https://my.comma.ai' + APPLE_REDIRECT_PATH;
+export const APPLE_OAUTH_PARAMS = {
+  client_id : APPLE_CLIENT_ID,
+  redirect_uri: APPLE_REDIRECT_URI,
+  response_type: 'code',
+  responseMode: 'query',
+};
+export const APPLE_REDIRECT_LINK = [APPLE_AUTH_ENDPOINT, qs.stringify(APPLE_OAUTH_PARAMS)].join('?')
 
-export const oauthRedirectLink = [GOOGLE_AUTH_ENDPOINT, qs.stringify(OAUTH_PARAMS)].join('?');
+export const GITHUB_CLIENT_ID = '2ca8e276e644c46c00fa';
+export const GITHUB_AUTH_ENDPOINT = 'https://github.com/login/oauth/authorize';
+export const GITHUB_REDIRECT_PATH = '/auth/h/redirect';
+export const GITHUB_REDIRECT_URI = redirectOrigin + GITHUB_REDIRECT_PATH;
+export const GITHUB_OAUTH_PARAMS = {
+  client_id: GITHUB_CLIENT_ID,
+  redirect_uri: GITHUB_REDIRECT_URI,
+};
+export const GITHUB_REDIRECT_LINK = [GITHUB_AUTH_ENDPOINT, qs.stringify(GITHUB_OAUTH_PARAMS)].join('?')
